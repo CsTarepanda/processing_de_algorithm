44f07a475892c0e2d115333d574f90370115edb9
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-07-09 14:33:02 +0900

    full_search@elixir

---
+++ full_search/Main.exs
@@ -0,0 +1,27 @@
+defmodule Main do
+  use Bitwise
+  def main(size) do
+    data = 0..size - 1 |> Enum.to_list
+    range = 1 <<< size
+    for x <- 0..range - 1 do
+      data |> calc(x, 0, size)
+    end |> Enum.map(& :io.format "~p, ", [&1])
+  end
+
+  def calc(data, pattern, sum, depth) do
+    if depth > 0 do
+      case pattern &&& 1 do
+        0 -> data
+          |> tl
+          |> calc(pattern >>> 1, sum, depth - 1)
+        1 -> data
+          |> tl
+          |> calc(pattern >>> 1, sum + hd(data), depth - 1)
+      end
+    else
+      sum
+    end
+  end
+end
+
+Main.main(6)
